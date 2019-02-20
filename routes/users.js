5f4a62b75e1b6be48ad8e073d28019136c3b5000
Joseph Frank <17801921+JoeyFrank@users.noreply.github.com>
2019-02-20 17:09:09 -0600

    Added password encryption framework

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,30 @@
 var express = require('express'),
-    database = require("../database");
+    database = require("../database");    
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 var router = express.Router();
 
-// Create new user
-router.post("/", (req, res, next) => {   
-    console.log(req.query);
+router.post("/", createUser);   
+router.put("/", updateUser);
+
+var createUser = function(req, res, next) {
+    //perform parsing/validation on query
+    var username, password, firstname, lastname;
+
+    //check if username alreadyexists
+
+    //encrypt password
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        //store password in database
+    });
+
     res.json("post users");
-});
+};
 
+var updateUser = function(req, res, next) {
 
-// Update user info
-router.put("/", (req, res, next) => {
-    res.json("put users");
-});
+};
 
 module.exports = router;
