25b4eeb991dd34b05ce6edaaef5127fe9272934b
Joseph Frank <17801921+JoeyFrank@users.noreply.github.com>
2019-03-13 14:31:44 -0500

    Moved friend images into individual directories

--- routes/people.js
+++ routes/people.js
@@ -7,7 +7,7 @@ var express = require('express'),
 var router = express.Router();
 var pool = database.pool;
 
-router.get("/");
+router.get("/", auth.verifyUser, getPeople);
 router.post("/", express.urlencoded({limit:'1mb', extended:false}), auth.verifyUser, createPerson);
 router.delete("/");
 
@@ -15,36 +15,31 @@ function createPerson(req, res){
     var firstname = req.body.firstname,
         lastname = req.body.lastname,
         base64Data = req.body.image;
-
     if(!(firstname && lastname && base64Data)) return res.status(400).json("Parameters Missing");
     
     var userId = res.locals.userId;
-    var filepath = "/srv/people/" + userId + "/" + shortid.generate() + ".png";
+    var friendsQuery = "INSERT INTO Friends (FriendFirst, FriendLast, UserID) VALUES (?,?,?)";
+    var friendTableQuery = "INSERT INTO FriendImage (FilePath, FriendID) VALUES (?,?)";
 
     try{
-        //Save friend image
-        fs.writeFile(filepath, base64Data, 'base64', function(err) {
+        //Friends entry
+        pool.query(friendsQuery, [firstname, lastname, userId], function (err, results, fields) {
             if(err) throw err;
-            // Log person in database
-            pool.getConnection(function(err, conn) {
-                if(err) throw err;
+            var friendId = results.insertId;
+            var friendDirectory = "/srv/people/" + userId + "/" + friendId;
 
-                var friendsQuery = "INSERT INTO Friends (FriendFirst, FriendLast, UserID) VALUES (?,?,?)";
-                var friendTableQuery = "INSERT INTO FriendImage (FilePath, FriendID) VALUES (?,?)";
-                
-                conn.query(friendsQuery, [firstname, lastname, userId], function (err, results, fields) {
-                    if(err){
-                        conn.release(); 
-                        throw err;
-                    }
+            //create friend directory
+            fs.mkdir(friendDirectory, function(err){
+                if(err) throw err;                   
+                var filepath = friendDirectory + "/" + shortid.generate() + ".png"; 
 
-                    var friendId = results.insertId;
+                //write image file
+                fs.writeFile(filepath , base64Data, 'base64', function(err) {
+                    if(err) throw err;
 
-                    conn.query(friendTableQuery, [filepath, friendId], function (err, results, fields) {
-                        conn.release(); 
+                    //FriendImage entry
+                    pool.query(friendTableQuery, [filepath, friendId], function (err, results, fields) { 
                         if(err) throw err;
-                        
-                        //person and image created
                         res.status(200).send("Person added successfully");
                     }); 
                 });
@@ -53,6 +48,21 @@ function createPerson(req, res){
     }catch(err){
         console.log(err);
         res.status(500).send("Application error");
+    }
+}
+
+function getPeople(req, res){
+    var userId = res.locals.userId;
+    var query = "SELECT FriendID, FriendFirst, FriendLast FROM Friends WHERE UserID = ?";
+
+    try{
+        pool.query(query, [userId], function (err, results, fields) {
+            if(err) throw err;
+            res.status(200).json(results);
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Database error");
     }
 }
 
