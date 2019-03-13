aa576f125d15454918758acad740d4a517254058
Joseph Frank <17801921+JoeyFrank@users.noreply.github.com>
2019-03-13 12:02:03 -0500

    Added favorite person creation functionality

--- routes/people.js
+++ routes/people.js
@@ -1,16 +1,66 @@
-var express = require('express');
+var express = require('express'),
+    auth = require('../auth/jwtAuth'),
+    fs = require('fs'),
+    shortid = require('shortid'),
+    database = require('../database');
+
 var router = express.Router();
+var pool = database.pool;
 
-router.get("/", (req, res, next) => {
-    res.json("get people");
-});
+router.get("/");
+router.post("/", auth.verifyUser, createPerson);
+router.delete("/");
 
-router.post("/", (req, res, next) => {
-    res.json("post people");
-});
+function createPerson(req, res){
+    /* 
+    - firstname, lastname, image in body
+    1. verify params
+    2. create person entry in db
+        - friend and friendimage tables need to be addressed 
+    3. write image to user folder in /srv/people/
+    */
 
-router.delete("/", (req, res, next) => {
-    res.json("delete people");
-});
+    var firstname = req.body.firstname,
+        lastname = req.body.lastname,
+        base64Data = req.body.image;
+
+    if(!(firstname && lastname && base64Data)) return res.status(400).json("Parameters Missing");
+    
+    var userId = res.locals.userId;
+    var filepath = "/srv/people/" + userId + "/" + shortid.generate() + ".png";
+
+    try{
+        fs.writeFile(filepath, base64Data, 'base64', function(err) {
+            if(err) throw err;
+            // Log person in database
+            pool.getConnection(function(err, conn) {
+                if(err) throw err;
+
+                var friendsQuery = "INSERT INTO Friends (FriendFirst, FriendLast, UserID) VALUES (?,?,?)";
+                var friendTableQuery = "INSERT INTO FriendImage (FilePath, FriendID) VALUES (?,?)";
+                
+                conn.query(friendsQuery, [firstname, lastname, userId], function (err, results, fields) {
+                    if(err){
+                        conn.release(); 
+                        throw err;
+                    }
+
+                    var friendId = results.insertId;
+
+                    conn.query(friendTableQuery, [filepath, friendId], function (err, results, fields) {
+                        conn.release(); 
+                        if(err) throw err;
+                        
+                        //person and image created
+                        res.status(200).send("Person added successfully");
+                    }); 
+                });
+            });
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Application error");
+    }
+}
 
 module.exports = router;
