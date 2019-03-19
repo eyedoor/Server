c3ec3a4308fa10bd4315d7daf9f983e4c5536afe
Joseph Frank <17801921+JoeyFrank@users.noreply.github.com>
2019-03-19 15:16:48 -0500

    Added friend deletion

--- routes/friends.js
+++ routes/friends.js
@@ -8,11 +8,11 @@ var router = express.Router();
 var pool = database.pool;
 const NUM_ALLOWED_FRIENDS = 100;
 
-router.get("/", auth.verifyUser, getPeople);
-router.post("/", express.urlencoded({limit:'1mb', extended:false}), auth.verifyUser, createPerson);
-router.delete("/", auth.verifyUser);
+router.get("/", auth.verifyUser, getFriends);
+router.post("/", express.urlencoded({limit:'1mb', extended:false}), auth.verifyUser, createFriend);
+router.delete("/", auth.verifyUser, deleteFriend);
 
-function createPerson(req, res){ 
+function createFriend(req, res){
     var firstname = req.body.firstname,
         lastname = req.body.lastname,
         base64Data = req.body.image;
@@ -58,7 +58,7 @@ function createPerson(req, res){
     }
 }
 
-function getPeople(req, res){
+function getFriends(req, res){
     var userId = res.locals.userId;
     var query = "SELECT FriendID, FriendFirst, FriendLast FROM Friends WHERE UserID = ?";
 
@@ -70,6 +70,47 @@ function getPeople(req, res){
     }catch(err){
         console.log(err);
         res.status(500).send("Database error");
+    }
+}
+
+function deleteFriend(req, res, next){
+    var friendId = req.query.friendId
+    if(!friendId) return res.status(400).json({ message: "Query Parameter Missing"});
+    
+    var userId = res.locals.userId;
+    var friendQuery = "SELECT FriendID FROM Friends WHERE UserID = ? AND FriendID = ?";
+    var friendImageQuery = "SELECT FilePath FROM FriendImage WHERE FriendID = ?";
+    var friendImageDeleteQuery = "DELETE FROM FriendImage WHERE FriendID = ?";
+    var friendDeleteQuery = "DELETE FROM Friends WHERE FriendID = ?";
+
+    try{  
+        pool.query(friendQuery, [userId, friendId], function (err, results, fields) {
+            if(err) throw err;
+            if(results.length == 0){
+                return res.status(404).json({ message: "Friend not found" });
+            }
+            pool.query(friendImageQuery, [friendId], function (err, results, fields) {
+                if(err) throw err;
+                if(results.length == 0){
+                    return res.status(404).json({ message: "Friend not found" });
+                }
+                // Return base64 encoding of .png image
+                fs.unlink(results[0].FilePath, function(err, data) {
+                    if(err) throw err;
+                    pool.query(friendImageDeleteQuery, [friendId], function (err, results, fields) {
+                        if(err) throw err;
+                        pool.query(friendDeleteQuery, [friendId], function (err, results, fields) {
+                            if(err) throw err;
+                            return res.status(200).json({ message: "Friend deleted" });
+                        });
+                    });
+                    
+                });
+            });
+        });
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message:"Application Error"});
     }
 }
 
